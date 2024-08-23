aad5a51bf0c83e8ff474f68e39cbcf1c9005313f
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-08-23 15:29:13 +0200

    styles done

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff1C2121);
+  static const Color onBoardingBackgroundColor = Color(0xfff9f9f9);
+  static const Color onBoardingButtonColor = Color(0xff007AFF);
+  static const Color backgroundColor = Color(0xfff2f2f6);
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Styles {
+  static TextStyle styleBold32 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.bold,
+      fontSize: 32.81,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleRegular14 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.96,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleBold17 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.bold, fontSize: 17, color: Colors.white);
+
+  static TextStyle styleSemiBold14 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 14.58,
+      color: const Color(0xff007AFF));
+
+  static TextStyle styleBold26 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.bold,
+      fontSize: 26,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleRegular13 = GoogleFonts.nunito(
+      fontWeight: FontWeight.w400,
+      fontSize: 13,
+      color: const Color(0xff3C3C43).withOpacity(.6));
+
+  static TextStyle styleRegularOpacity17 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w400,
+      fontSize: 17.68,
+      color: const Color(0xff1C2121).withOpacity(.6));
+
+  static TextStyle styleBold15 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.bold, fontSize: 15.62, color: Colors.black);
+
+  static TextStyle styleSemiBold15 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 15,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleLight12 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w300,
+      fontSize: 12,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleSemiBold9 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 8.9,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleSemiBold26 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 16.52,
+      color: const Color(0xff828282));
+
+  static TextStyle styleRegular12 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w400,
+      fontSize: 12,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleRegular17 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w400,
+      fontSize: 17,
+      color: const Color(0xff1C2121));
+
+  static TextStyle styleItalic17 = GoogleFonts.nunitoSans(
+      fontWeight: FontWeight.w400,
+      fontSize: 17,
+      fontStyle: FontStyle.italic,
+      color: const Color(0xff1C2121));
+}
