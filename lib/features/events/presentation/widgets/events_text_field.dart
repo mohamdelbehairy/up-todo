ba89373f34ce82661736ee5eebdfb7601ef002cd
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-09-14 19:45:48 +0300

    app is done

--- lib/features/events/presentation/widgets/events_text_field.dart
+++ lib/features/events/presentation/widgets/events_text_field.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:up_todo/core/utils/colors.dart';
 import 'package:up_todo/core/utils/styles.dart';
 
 import '../../../create_note/data/models/text_field_model.dart';
+
 
 class EventsTextField extends StatelessWidget {
   const EventsTextField({super.key, required this.textFieldModel});
@@ -13,15 +13,17 @@ class EventsTextField extends StatelessWidget {
     return TextFormField(
       controller: textFieldModel.controller,
       validator: textFieldModel.validator,
-      style: Styles.styleBold26.copyWith(fontSize: 14),
+      style: Styles.styleBold17,
       decoration: InputDecoration(
+        contentPadding: const EdgeInsets.only(left: 8),
         hintText: textFieldModel.title,
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: AppColors.primaryColor)),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: AppColors.primaryColor)),
+        border: InputBorder.none,
+        // border: OutlineInputBorder(
+        //     borderRadius: BorderRadius.circular(8),
+        //     borderSide: const BorderSide(color: AppColors.primaryColor)),
+        // focusedBorder: OutlineInputBorder(
+        //     borderRadius: BorderRadius.circular(8),
+        //     borderSide: const BorderSide(color: AppColors.primaryColor)),
       ),
     );
   }
