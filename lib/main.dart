59c7b2dfc53dd3725d41a57cb9f521f00d4a50d9
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-09-14 18:11:52 +0300

    when app route the app don't route

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:up_todo/core/widgets/up_todo.dart';
 import 'core/utils/function/init_main.dart';
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   await initMain();
 
   runApp(const UpTodo());
 }
-
-
